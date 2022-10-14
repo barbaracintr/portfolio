852c563367111fef33e28d1314732b4c90a0d84a
Bárbara Cintra <b.mcgomes@hotmail.com>
2022-10-14 13:30:49 -0400

    update-portfolio-1.7

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -17,7 +17,7 @@ export const Projects = () => {
     {
       title: "To do list",
       img: "https://img.freepik.com/fotos-gratis/vista-traseira-do-programador-trabalhando-a-noite-toda_1098-18697.jpg?w=900&t=st=1665101829~exp=1665102429~hmac=47f19069ca1e872bb57ead399567d4da081abd3fe1348836bcb553b3705d9438",
-      link: "https://todo-list-delta-olive.vercel.app/",
+      link: "https://todolist-project-liart.vercel.app",
       gh: "https://github.com/barbaracintr/todolist-project"
     },
     {
